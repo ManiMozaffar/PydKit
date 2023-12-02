456e54a41138b20852f653f8b24e8598e276c5e1
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2023-12-02 18:04:49 +0330

    comment out writer functions

--- pydkit/csv.py
+++ pydkit/csv.py
@@ -14,50 +14,9 @@ StrNone = Annotated[None, BeforeValidator(lambda v: None if v == "" else v)]
 """turn empty string to None, this is default behavior by csv built in package in python"""
 
 
-# def serialize(csv_str: str, model_type: Type[T]) -> list[T]:
-#     """Serialize CSV into list of models"""
-#     reader = DictReader(csv_str.splitlines())
-#     result = [model_type(**row) for row in reader]
-#     return result
-
-
-def deserialize(models: Sequence[BaseModel]) -> str:
-    """Deserialize pydantic model to CSV representation string"""
-    output = StringIO()
-    writer = DictWriter(output, fieldnames=models[0].model_fields.keys())
-    writer.writeheader()
-    for model in models:
-        writer.writerow(model.model_dump())
-    return output.getvalue()
-
-
-async def save_async(location: PathLike | str, models: Sequence[BaseModel]) -> None:
-    """Saves the model to the given location as csv"""
-    output = deserialize(models)
-    async with aiofiles.open(location, mode="w") as file:
-        await file.write(output)
-    return None
-
-
-# async def read_async(location: PathLike | str, model_type: Type[T]) -> list[T]:
-#     """Create models from to the given location as csv"""
-#     async with aiofiles.open(location, mode="r", newline="") as file:
-#         result = serialize(await file.read(), model_type)
-#         return result
-
-
-def save(location: PathLike | str, models: Sequence[BaseModel]) -> None:
-    """Saves the model to the given location as csv"""
-    return syncify(save_async)(location=location, models=models)
-
-
-# def read(location: PathLike | str, model_type: Type[T]) -> list[T]:
-#     """Create models from to the given location as csv"""
-#     return syncify(read_async)(location=location, model_type=model_type)
-
 # Newer implementation and design decisions to make the pydkit.csv module
 # fully compatible with python standard library `csv` module
-# All the `csv` module APIs will be reserved + some modifications to gain Pydantic features
+# Almost all the `csv` module APIs will be reserved + some modifications to gain Pydantic features
 
 # Reader utilities
 
@@ -65,7 +24,6 @@ def save(location: PathLike | str, models: Sequence[BaseModel]) -> None:
 # docs:
 #   https://docs.python.org/3/library/csv.html#csv.reader
 #   https://docs.python.org/3/library/csv.html#reader-objects
-
 
 def reader(csvfile: Iterable[str], model: Type[T], dialect="excel", **fmtparams) -> "_Reader":
     """Return a reader object similar to Python `csv._reader`"""
@@ -117,6 +75,7 @@ class DictReader:
             data[self.dict_reader.restkey] = restkey_value
         return data
 
+
 class _Reader:
     """Mimic the csv._reader object attributes and behaviour"""
 
@@ -145,3 +104,45 @@ class _Reader:
     @property
     def line_num(self):
         return self.reader_object.line_num
+
+
+# def serialize(csv_str: str, model_type: Type[T]) -> list[T]:
+#     """Serialize CSV into list of models"""
+#     reader = DictReader(csv_str.splitlines())
+#     result = [model_type(**row) for row in reader]
+#     return result
+
+
+# def deserialize(models: Sequence[BaseModel]) -> str:
+#     """Deserialize pydantic model to CSV representation string"""
+#     output = StringIO()
+#     writer = DictWriter(output, fieldnames=models[0].model_fields.keys())
+#     writer.writeheader()
+#     for model in models:
+#         writer.writerow(model.model_dump())
+#     return output.getvalue()
+
+
+# async def save_async(location: PathLike | str, models: Sequence[BaseModel]) -> None:
+#     """Saves the model to the given location as csv"""
+#     output = deserialize(models)
+#     async with aiofiles.open(location, mode="w") as file:
+#         await file.write(output)
+#     return None
+
+
+# async def read_async(location: PathLike | str, model_type: Type[T]) -> list[T]:
+#     """Create models from to the given location as csv"""
+#     async with aiofiles.open(location, mode="r", newline="") as file:
+#         result = serialize(await file.read(), model_type)
+#         return result
+
+
+# def save(location: PathLike | str, models: Sequence[BaseModel]) -> None:
+#     """Saves the model to the given location as csv"""
+#     return syncify(save_async)(location=location, models=models)
+
+
+# def read(location: PathLike | str, model_type: Type[T]) -> list[T]:
+#     """Create models from to the given location as csv"""
+#     return syncify(read_async)(location=location, model_type=model_type)
