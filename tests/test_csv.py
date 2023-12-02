fd6d1b4826cdb76dabe6f39e69caf098410f55ed
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2023-12-02 17:29:56 +0330

    test the `pydkit.DictReader`

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -5,7 +5,7 @@ from tempfile import NamedTemporaryFile
 import pytest
 from pydantic import BaseModel
 
-from pydkit.csv import StrNone, reader
+from pydkit.csv import StrNone, reader, DictReader
 
 
 class User(BaseModel):
@@ -16,17 +16,17 @@ class User(BaseModel):
 
 
 @pytest.mark.parametrize(
-    ['to_write', 'expected'],
+    ["to_write", "expected"],
     [
         ([1, "Zohre", 30, "female"], [1, "Zohre", 30, "female"]),
         ([2, "Mahdi", 25, "male"], [2, "Mahdi", 25, "male"]),
         ([3, "Alice", 25, None], [3, "Alice", 25, None]),
         ([4, "John", 33, ""], [4, "John", 33, None]),
         ([5, "Mary", 36], [5, "Mary", 36, None]),
-    ]
+    ],
 )
 def test_correct_data_reader(to_write, expected):
-    """Test pydkit.csv.reader with a temporary file"""
+    """Test pydkit.csv.reader"""
     with NamedTemporaryFile(mode="w", delete=False) as tf:
         writer = csv.writer(tf)
         writer.writerow(["id", "name", "age", "gender"])
@@ -37,6 +37,29 @@ def test_correct_data_reader(to_write, expected):
             kit_reader = reader(csvfile, model=User)
             next(kit_reader)  # header
             assert expected == next(kit_reader)
+
+
+@pytest.mark.parametrize(
+    ["to_write", "expected"],
+    [
+        ([1, "Zohre", 30, "female"], {"id": 1, "name": "Zohre", "age": 30, "gender": "female"}),
+        ([2, "Mahdi", 25, "male"], {"id": 2, "name": "Mahdi", "age": 25, "gender": "male"}),
+        ([3, "Alice", 25, None], {"id": 3, "name": "Alice", "age": 25, "gender": None}),
+        ([4, "John", 33, ""], {"id": 4, "name": "John", "age": 33, "gender": None}),
+        ([5, "Mary", 36], {"id": 5, "name": "Mary", "age": 36, "gender": None}),
+    ],
+)
+def test_dict_reader(to_write, expected):
+    """Test pydkit.DictReader"""
+    with NamedTemporaryFile(mode="w", delete=False) as tf:
+        writer = csv.writer(tf)
+        writer.writerow(["id", "name", "age", "gender"])
+        writer.writerow(to_write)
+        tf.close()
+
+        with open(tf.name) as csvfile:
+            dict_reader = DictReader(csvfile, User)
+            assert expected == next(dict_reader)
 
 
 # @pytest.mark.asyncio
