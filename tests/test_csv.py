041a2a605df60eba5422c064247b1ca213435c46
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2023-12-02 18:58:23 +0330

    test the `writer` function

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -3,9 +3,9 @@ from typing import Literal
 from tempfile import NamedTemporaryFile
 
 import pytest
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 
-from pydkit.csv import StrNone, reader, DictReader
+from pydkit.csv import StrNone, reader, DictReader, writer
 
 
 class User(BaseModel):
@@ -25,12 +25,12 @@ class User(BaseModel):
         (["5", "Mary", "36"], [5, "Mary", 36, None]),
     ],
 )
-def test_correct_data_reader(to_write, expected):
+def test_reader(to_write, expected):
     """Test pydkit.csv.reader"""
     with NamedTemporaryFile(mode="w", delete=False) as tf:
-        writer = csv.writer(tf)
-        writer.writerow(["id", "name", "age", "gender"])
-        writer.writerow(to_write)
+        writer_ = csv.writer(tf)
+        writer_.writerow(["id", "name", "age", "gender"])
+        writer_.writerow(to_write)
         tf.close()
 
         with open(tf.name) as csvfile:
@@ -52,11 +52,116 @@ def test_correct_data_reader(to_write, expected):
 def test_dict_reader(to_write, expected):
     """Test pydkit.DictReader"""
     with NamedTemporaryFile(mode="w", delete=False) as tf:
-        writer = csv.writer(tf)
-        writer.writerow(["id", "name", "age", "gender"])
-        writer.writerow(to_write)
+        writer_ = csv.writer(tf)
+        writer_.writerow(["id", "name", "age", "gender"])
+        writer_.writerow(to_write)
         tf.close()
 
         with open(tf.name) as csvfile:
             dict_reader = DictReader(csvfile, User)
             assert expected == next(dict_reader)
+
+
+def test_writer_writerow_model():
+    """Test pydkit.writer.writerow"""
+    with NamedTemporaryFile(mode="w", delete=False) as tf:
+        header = ["id", "name", "age", "gender"]
+        kit_writer = writer(tf, User)
+        kit_writer.writerow(header, header=True)
+
+        user = User(id=1, name="Alice", age=30, gender="female")
+        kit_writer.writerow(user)
+
+        tf.close()
+
+        with open(tf.name) as csvfile:
+            kit_reader = reader(csvfile, User)
+            next(kit_reader)  # header
+            assert [1, "Alice", 30, "female"] == next(kit_reader)
+
+
+def test_writer_writerow_raw_row():
+    """Test pydkit.writer.writerow"""
+    with NamedTemporaryFile(mode="w", delete=False) as tf:
+        header = ["id", "name", "age", "gender"]
+        kit_writer = writer(tf, User)
+        kit_writer.writerow(header, header=True)
+
+        user = [1, "Alice", 30, "female"]
+        kit_writer.writerow(user)
+
+        tf.close()
+
+        with open(tf.name) as csvfile:
+            kit_reader = reader(csvfile, User)
+            next(kit_reader)  # header
+            assert [1, "Alice", 30, "female"] == next(kit_reader)
+
+
+def test_writer_writerow_raw_row_incorrect_data():
+    with NamedTemporaryFile(mode="w", delete=False) as tf:
+        header = ["id", "name", "age", "gender"]
+        kit_writer = writer(tf, User)
+        kit_writer.writerow(header, header=True)
+
+        user = [1, "Alice", 30, "femail"]
+        with pytest.raises(ValidationError):
+            kit_writer.writerow(user)
+
+
+def test_writer_writerows_model():
+    """Test pydkit.writer.writerows"""
+    with NamedTemporaryFile(mode="w", delete=False) as tf:
+        header = ["id", "name", "age", "gender"]
+        kit_writer = writer(tf, User)
+        kit_writer.writerow(header, header=True)
+
+        users = [
+            User(id=1, name="Alice", age=30, gender="female"),
+            User(id=2, name="Mahdi", age=21, gender="male"),
+        ]
+        kit_writer.writerows(users)
+
+        tf.close()
+
+        with open(tf.name) as csvfile:
+            kit_reader = reader(csvfile, User)
+            next(kit_reader)  # header
+            assert [1, "Alice", 30, "female"] == next(kit_reader)
+            assert [2, "Mahdi", 21, "male"] == next(kit_reader)
+
+
+def test_writer_writerows_raw_row():
+    """Test pydkit.writer.writerows"""
+    with NamedTemporaryFile(mode="w", delete=False) as tf:
+        header = ["id", "name", "age", "gender"]
+        kit_writer = writer(tf, User)
+        kit_writer.writerow(header, header=True)
+
+        users = [
+            [1, "Alice", 30, "female"],
+            [2, "Mahdi", 21, "male"],
+        ]
+        kit_writer.writerows(users)
+
+        tf.close()
+
+        with open(tf.name) as csvfile:
+            kit_reader = reader(csvfile, User)
+            next(kit_reader)  # header
+            assert [1, "Alice", 30, "female"] == next(kit_reader)
+            assert [2, "Mahdi", 21, "male"] == next(kit_reader)
+
+
+def test_writer_writerows_raw_row_incorrect_data():
+    with NamedTemporaryFile(mode="w", delete=False) as tf:
+        header = ["id", "name", "age", "gender"]
+        kit_writer = writer(tf, User)
+        kit_writer.writerow(header, header=True)
+
+        users = [
+            [1, "Alice", 30, "female"],
+            [2, "Mahdi", 21, "mail"],
+        ]
+        with pytest.raises(ValidationError):
+            kit_writer.writerows(users)
